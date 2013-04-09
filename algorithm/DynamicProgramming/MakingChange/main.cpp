ff8b9635a114e4fb7818f8f9b2567f8109e7b6c2
michael <michael@mmics.(none)>
2013-04-10 00:41:09 +0800

    MakingChange: 2 dimension solution

--- algorithm/DynamicProgramming/MakingChange/main.cpp
+++ algorithm/DynamicProgramming/MakingChange/main.cpp
@@ -28,6 +28,44 @@ int makingChange(int *coin, int n, int S)
         return count[S];
 }
 
+int makingChange_2Dim(int *coin, int n, int S)
+{
+    //Base case
+    if (n < 0)
+        return -1;
+    if (n == 0 && S > 0)
+        return -1;
+
+    int count[n + 1][S + 1];
+
+    //no coin, impossible
+    for (int i = 0; i < n + 1; i++)
+        count[0][i] = 0xffff;
+
+    //S = 0, no need coin
+    for (int i = 0; i < S + 1; i++)
+        count[i][0] = 0;
+
+    count[0][0] = 0;
+
+    for (int i = 1; i < n + 1; i++) {
+        for (int j = 1; j < S + 1; j++) {
+            int cur_value = j;
+            int cur_coin = coin[i - 1];
+            if (cur_coin <= cur_value) {
+                count[i][j] = min(count[i - 1][j], count[i][cur_value - cur_coin] + 1);
+            }
+            else
+                count[i][j] = count[i - 1][j];
+        }
+    }
+
+    if (count[n][S] == 0xffff)
+        return -1;
+    else
+        return count[n][S];
+}
+
 int main(int argc, char **argv)
 {
     int size, sum;
@@ -50,9 +88,15 @@ int main(int argc, char **argv)
 
     cout << endl;
     if (out > 0)
-        cout << "Minimum " << out << " coins needed" << endl;
+        cout << "1 Dim: Minimum " << out << " coins needed" << endl;
     else
-        cout << "Impossible" << endl;
+        cout << "1 Dim: Impossible" << endl;
 
+    out = makingChange_2Dim(arr, size, sum);
+
+    if (out > 0)
+        cout << "2 Dim: Minimum " << out << " coins needed" << endl;
+    else
+        cout << "2 Dim: Impossible" << endl;
     delete [] arr;
 }
